585d31d9a427dd5a6a072bd1d1febf7cf98a889a
poicoLaptop <rodrigodecgomes@gmail.com>
2024-01-04 15:58:33 +0000

    Change semaphores to condition variables

--- proj_23-24-p2_base/server/main.c
+++ proj_23-24-p2_base/server/main.c
@@ -1,22 +1,21 @@
+#include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <semaphore.h>
-#include <string.h>
 
 #include "common/constants.h"
 #include "common/io.h"
 #include "common/messages.h"
 #include "eventlist.h"
 #include "operations.h"
-
-#include <errno.h>
 
 #define BUFFER_SIZE 4
 
@@ -33,7 +32,7 @@ void handle_create(int req_fd, int resp_fd);
 void handle_reserve(int req_fd, int resp_fd);
 void handle_show(int req_fd, int resp_fd);
 void handle_list(int req_fd, int resp_fd);
-void *worker_thread_main(void *arg);
+void* worker_thread_main(void* arg);
 
 unsigned int state_access_delay_us;
 int registerFIFO;
@@ -48,10 +47,11 @@ setup_request buffer[BUFFER_SIZE];
 int in = 0;
 int out = 0;
 
-sem_t empty;  // semaphore to track empty slots in buffer
-sem_t full;   // semaphore to track filled slots in buffer
+pthread_mutex_t buffer_mutex;
+pthread_cond_t buffer_not_full;
+pthread_cond_t buffer_not_empty;
 
-volatile char show_flag = 0; //no need for thread safety, not important
+volatile char show_flag = 0;  // no need for thread safety, not important
 
 int main(int argc, char* argv[]) {
   int ret = parse_args(argc, argv);
@@ -73,18 +73,16 @@ int main(int argc, char* argv[]) {
   while (!server_should_quit) {
     accept_client();
 
-    if (show_flag)
-    {
+    if (show_flag) {
       show_flag = 0;
 
       size_t count;
-      unsigned int *data = ems_list_events_to_client(&count);
+      unsigned int* data = ems_list_events_to_client(&count);
 
       if (data == NULL) continue;
 
       write(1, "Listing all events:\n", 20);
-      for (size_t i = 0; i < count; i++)
-      {
+      for (size_t i = 0; i < count; i++) {
         char buff[32];
         snprintf(buff, 32, "Event %d:\n", data[i]);
         write(1, buff, strlen(buff));
@@ -92,7 +90,7 @@ int main(int argc, char* argv[]) {
       }
     }
   }
-  
+
   printf("Close server.\n");
   close_server();
 }
@@ -127,8 +125,9 @@ void init_server() {
   mkfifo(FIFO_path, 0666);
   registerFIFO = open(FIFO_path, O_RDWR);
 
-  sem_init(&empty, 0, BUFFER_SIZE);
-  sem_init(&full, 0, 0);
+  pthread_mutex_init(&buffer_mutex, NULL);
+  pthread_cond_init(&buffer_not_full, NULL);
+  pthread_cond_init(&buffer_not_empty, NULL);
 
   for (int i = 0; i < MAX_SESSION_COUNT; i++) {
     thread_args[i] = (unsigned int)i;
@@ -138,16 +137,15 @@ void init_server() {
 }
 
 void accept_client() {
-  
   char opcode;
-  if(read(registerFIFO, &opcode, sizeof(char)) == -1)
-  {
-    if (errno == 4) //Interrupted system call
-    { return; } //ignore
+  if (read(registerFIFO, &opcode, sizeof(char)) == -1) {
+    if (errno == 4)  // Interrupted system call
+    {
+      return;
+    }  // ignore
     fprintf(stderr, "Error reading from pipe while accepting client: %d.\n", errno);
     exit(1);
   }
-  
 
   setup_request request;
   if (read(registerFIFO, &request, sizeof(setup_request)) == -1) {
@@ -156,15 +154,21 @@ void accept_client() {
   }
 
   printf("Producer is producing...\n");
-  sem_wait(&empty);  // decrement empty count
+  pthread_mutex_lock(&buffer_mutex);
+  // Wait for buffer to not be full
+  while ((in + 1) % BUFFER_SIZE == out) pthread_cond_wait(&buffer_not_full, &buffer_mutex);
+  // Add request to buffer
   buffer[in] = request;
   in = (in + 1) % BUFFER_SIZE;
-  sem_post(&full);  // increment count of full slots
+  // Signal that buffer is not empty
+  pthread_cond_signal(&buffer_not_empty);
+  // Unlock mutex
+  pthread_mutex_unlock(&buffer_mutex);
+
   printf("Producer produced.\n");
 }
 
-void *worker_thread_main(void *arg)
-{
+void* worker_thread_main(void* arg) {
   printf("Thread start.\n");
   unsigned int session_id = *((unsigned int*)arg);
 
@@ -172,27 +176,30 @@ void *worker_thread_main(void *arg)
   sigemptyset(&sigset);
   sigaddset(&sigset, SIGUSR1);
   pthread_sigmask(SIG_BLOCK, &sigset, NULL);
-  
+
   while (1) {
-    sem_wait(&full);  // decrement full count
+    pthread_mutex_lock(&buffer_mutex);
+    while (in == out) pthread_cond_wait(&buffer_not_empty, &buffer_mutex);
     setup_request request = buffer[out];
+    out = (out + 1) % BUFFER_SIZE;
 
     printf("Consumer is consuming...\n");
     out = (out + 1) % BUFFER_SIZE;
-    sem_post(&empty);  // increment count of empty slots
+    // Signal that buffer is not full
+    pthread_cond_signal(&buffer_not_full);
+    // Unlock mutex
+    pthread_mutex_unlock(&buffer_mutex);
     printf("Consumer consumed.\n");
 
     printf("Received paths '%s' and '%s'.\n", request.request_fifo_name, request.response_fifo_name);
 
     int req_fd, resp_fd;
-    
-    if((req_fd = open(request.request_fifo_name, O_RDONLY)) == -1)
-    {
+
+    if ((req_fd = open(request.request_fifo_name, O_RDONLY)) == -1) {
       fprintf(stderr, "Error opening request pipe\n");
       exit(1);
     }
-    if((resp_fd = open(request.response_fifo_name, O_WRONLY)) == -1)
-    {
+    if ((resp_fd = open(request.response_fifo_name, O_WRONLY)) == -1) {
       fprintf(stderr, "Error opening response pipe\n");
       exit(1);
     }
@@ -203,18 +210,16 @@ void *worker_thread_main(void *arg)
 
 // Each worker thread enters this function once for each client
 void handle_client(unsigned int session_id, int req_fd, int resp_fd) {
-  setup_response resp = { .session_id = session_id };
-  
-  if(write(resp_fd, &resp, sizeof(setup_response))== -1)
-  {
+  setup_response resp = {.session_id = session_id};
+
+  if (write(resp_fd, &resp, sizeof(setup_response)) == -1) {
     fprintf(stderr, "Error writing to pipe\n");
     exit(1);
   }
 
   int should_work = 1;
 
-  while (should_work)
-  {
+  while (should_work) {
     should_work = process_command(req_fd, resp_fd);
   }
 
@@ -241,6 +246,9 @@ void close_server() {
     fprintf(stderr, "Error closing register FIFO\n");
     exit(1);
   }
+  pthread_mutex_destroy(&buffer_mutex);
+  pthread_cond_destroy(&buffer_not_full);
+  pthread_cond_destroy(&buffer_not_empty);
   close_server_threads();
   ems_terminate();
   exit(0);
@@ -263,8 +271,7 @@ void close_server_threads() {
 
 int process_command(int req_fd, int resp_fd) {
   core_request core;
-  if(read(req_fd, &core, sizeof(core_request)) == -1)
-  {
+  if (read(req_fd, &core, sizeof(core_request)) == -1) {
     fprintf(stderr, "Error reading from pipe while reading core: %d.\n", errno);
     exit(1);
   }
@@ -324,7 +331,7 @@ void handle_reserve(int req_fd, int resp_fd) {
   size_t* ys = malloc(req.num_seats * sizeof(size_t));
   if (read(req_fd, xs, req.num_seats * sizeof(size_t)) == -1) {
     fprintf(stderr, "Error reading from pipe\n");
-    free(xs); // If this fails, we need to free xs and ys before exiting
+    free(xs);  // If this fails, we need to free xs and ys before exiting
     free(ys);
     exit(1);
   }
@@ -357,7 +364,7 @@ void handle_show(int req_fd, int resp_fd) {
 
   size_t rows = 0, cols = 0;
   unsigned int* data = ems_show_to_client(req.event_id, &rows, &cols);
-  
+
   show_response resp;
   resp.num_cols = cols;
   resp.num_rows = rows;
@@ -369,13 +376,12 @@ void handle_show(int req_fd, int resp_fd) {
     exit(1);
   }
 
-  if(write(resp_fd, data, sizeof(unsigned int) * rows * cols) == -1)
-  {
+  if (write(resp_fd, data, sizeof(unsigned int) * rows * cols) == -1) {
     fprintf(stderr, "Error writing to pipe\n");
     free(data);
     exit(1);
   }
-  
+
   free(data);
 }
 
@@ -385,7 +391,7 @@ void handle_list(int req_fd, int resp_fd) {
 
   size_t event_count = 0;
   unsigned int* data = ems_list_events_to_client(&event_count);
-  
+
   list_response resp;
   resp.num_events = event_count;
   resp.return_code = data == NULL ? 1 : 0;
@@ -395,8 +401,7 @@ void handle_list(int req_fd, int resp_fd) {
     exit(1);
   }
 
-  if(write(resp_fd, data, event_count * sizeof(unsigned int)) == -1)
-  {
+  if (write(resp_fd, data, event_count * sizeof(unsigned int)) == -1) {
     fprintf(stderr, "Error writing to pipe\n");
     free(data);
     exit(1);
