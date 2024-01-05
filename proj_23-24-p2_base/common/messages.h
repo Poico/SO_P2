0ce839f5f85361f0799f93b7bcb17ddc1f302dc4
poicoLaptop <rodrigodecgomes@gmail.com>
2024-01-05 13:47:31 +0000

    More comments

--- proj_23-24-p2_base/common/messages.h
+++ proj_23-24-p2_base/common/messages.h
@@ -3,68 +3,73 @@
 
 #include <sys/types.h>
 
+// Enumeration of message opcodes
 enum MSG_OPCODE
 {
-	MSG_SETUP = 1,
-	MSG_QUIT = 2,
-	MSG_CREATE  = 3,
-	MSG_RESERVE = 4,
-	MSG_SHOW = 5,
-	MSG_LIST = 6
+	MSG_SETUP = 1,    // Opcode for setup message
+	MSG_QUIT = 2,     // Opcode for quit message
+	MSG_CREATE  = 3,  // Opcode for create message
+	MSG_RESERVE = 4,  // Opcode for reserve message
+	MSG_SHOW = 5,     // Opcode for show message
+	MSG_LIST = 6      // Opcode for list message
 };
 
+// Structure for core request message
 typedef struct {
-	char opcode;
-	unsigned int session_id;
+	char opcode;             // Opcode of the request
+	unsigned int session_id; // Session ID
 } __attribute__((packed)) core_request;
 
+// Structure for setup request message
 typedef struct {
-	char request_fifo_name[40];
-	char response_fifo_name[40];
+	char request_fifo_name[40];   // Name of the request FIFO
+	char response_fifo_name[40];  // Name of the response FIFO
 } __attribute__((packed)) setup_request;
 
+// Structure for setup response message
 typedef struct {
-	unsigned int session_id;
+	unsigned int session_id;  // Session ID
 } __attribute__((packed)) setup_response;
 
-//NOT NEEDED: quit_request
-
-//NOT NEEDED: quit_response
-
+// Structure for create request message
 typedef struct {
-	unsigned int event_id;
-	size_t num_rows;
-	size_t num_cols;
+	unsigned int event_id;  // Event ID
+	size_t num_rows;        // Number of rows
+	size_t num_cols;        // Number of columns
 } __attribute__((packed)) create_request;
 
+// Structure for create response message
 typedef struct {
-	int return_code;
+	int return_code;  // Return code
 } __attribute__((packed)) create_response;
 
+// Structure for reserve request message
 typedef struct {
-	unsigned int event_id;
-	size_t num_seats;
+	unsigned int event_id;  // Event ID
+	size_t num_seats;       // Number of seats to reserve
 } __attribute__((packed)) reserve_request;
 
+// Structure for reserve response message
 typedef struct {
-	int return_code;
+	int return_code;  // Return code
 } __attribute__((packed)) reserve_response;
 
+// Structure for show request message
 typedef struct {
-	unsigned int event_id;
+	unsigned int event_id;  // Event ID
 } __attribute__((packed)) show_request;
 
+// Structure for show response message
 typedef struct {
-	int return_code;
-	size_t num_rows;
-	size_t num_cols;
+	int return_code;   // Return code
+	size_t num_rows;   // Number of rows
+	size_t num_cols;   // Number of columns
 } __attribute__((packed)) show_response;
 
-//NOT NEEDED: list_request
-
+// Structure for list response message
 typedef struct {
-	int return_code;
-	size_t num_events;
+	int return_code;    // Return code
+	size_t num_events;  // Number of events
 } __attribute__((packed)) list_response;
 
 #endif
